49c81f217281b73bea879dc82fc3aa32ac9e2ead
Juan Dorado <fdorado985@gmail.com>
2020-10-30 20:55:01 -0600

    Map the transactions to create a list of cards

--- lib/main.dart
+++ lib/main.dart
@@ -47,9 +47,12 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            color: Colors.red,
-            child: Text('LIST OF TX'),
+          Column(
+            children: transactions.map((transaction) {
+              return Card(
+                child: Text(transaction.title),
+              );
+            }).toList(),
           ),
         ],
       ),
