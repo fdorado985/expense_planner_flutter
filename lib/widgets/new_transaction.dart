b200da3c53df8c23d9f790a0ee4ffd04c8676e47
Juan Dorado <fdorado985@gmail.com>
2020-10-31 00:18:39 -0600

    Refactor the function to submit new transactions, change the amount keyboard to just numbers and add the submited function

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -22,10 +22,11 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => _submitData,
             ),
             FlatButton(
-              onPressed: () => onPressedNewTransaction(
-                  titleController.text, double.parse(amountController.text)),
+              onPressed: _submitData,
               child: Text('Add Transaction'),
               textColor: Colors.purple,
             ),
@@ -33,5 +34,15 @@ class NewTransaction extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+    onPressedNewTransaction(enteredTitle, enteredAmount);
   }
 }
