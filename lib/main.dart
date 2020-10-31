76796e8af8a9fe84baf91b97cc880da2ff72a59f
Juan Dorado <fdorado985@gmail.com>
2020-10-30 21:23:14 -0600

    Using the international language package change how the date looks on the transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expense_planner_flutter/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -84,7 +85,7 @@ class MyHomePage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          transaction.date.toString(),
+                          DateFormat.yMMMd().format(transaction.date),
                           style: TextStyle(
                             color: Colors.grey,
                           ),
