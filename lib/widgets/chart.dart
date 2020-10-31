1938ee1738174d9eed42b2146a9ec874b347d141
Juan Dorado <fdorado985@gmail.com>
2020-10-31 10:46:34 -0600

    Create the logic to calculate the chart content of week and amount per day

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+  List<Map<String, Object>> get _groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+      double totalSum = 0.0;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+
+      return {'day': DateFormat.E(weekDay), 'amount': totalSum};
+    });
+  }
+
+  Chart({@required this.recentTransactions});
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      elevation: 6.0,
+      margin: EdgeInsets.all(6.0),
+      child: Row(
+        children: [],
+      ),
+    );
   }
 }
