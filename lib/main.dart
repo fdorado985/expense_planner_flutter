22161a1a4dd6139ba3207bfb51a9e55c0575e431
Juan Dorado <fdorado985@gmail.com>
2020-10-31 15:46:49 -0600

    Update the function to be able to use the selected date

--- lib/main.dart
+++ lib/main.dart
@@ -111,12 +111,12 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  void _addNewTransaction(String title, double amount) {
+  void _addNewTransaction(String title, double amount, DateTime chosenDate) {
     final newTransaction = Transaction(
       id: DateTime.now().toString(),
       title: title,
       amount: amount,
-      date: DateTime.now(),
+      date: chosenDate,
     );
 
     setState(() {
--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -66,14 +66,17 @@ class _NewTransactionState extends State<NewTransaction> {
   }
 
   void _submitData() {
+    if (_amountController.text.isEmpty) {
+      return;
+    }
     final enteredTitle = _titleController.text;
     final enteredAmount = double.parse(_amountController.text);
 
-    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+    if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectedDate == null) {
       return;
     }
 
-    widget.onPressedNewTransaction(enteredTitle, enteredAmount);
+    widget.onPressedNewTransaction(enteredTitle, enteredAmount, _selectedDate);
 
     Navigator.of(context).pop();
   }
